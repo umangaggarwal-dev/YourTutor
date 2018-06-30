4ccb1003ffb6282a929a56d8b8592ff3b3353de4
FlauistUmang <umangaggarwal31@gmail.com>
2018-06-30 16:07:09 +0530

    Mptt

---
+++ YourTutor/coglearn/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import *
+
+"""class SignupForm(forms.Form):
+    firstName = forms.CharField(max_length = 50, required=True, widget=forms.TextInput)
+    lastName = forms.CharField(max_length = 50)
+    email = forms.CharField
+"""
--- YourTutor/coglearn/models.py
+++ YourTutor/coglearn/models.py
@@ -1,5 +1,33 @@
 from __future__ import unicode_literals
 from django.db import models
+from mptt.fields import TreeForeignKey
+from mptt.models import MPTTModel
+import os
+
+class Student(models.Model):
+    firstName = models.CharField(max_length = 50)
+    lastName = models.CharField(max_length = 50)
+    email = models.EmailField()
+    school = models.CharField(max_length = 100)
+
+
+class Teacher(models.Model):
+    firstName = models.CharField(max_length=50)
+    lastName = models.CharField(max_length=50)
+    email = models.EmailField()
+
+
+class Topic(MPTTModel):
+    title = models.CharField(max_length=100)
+    parent = TreeForeignKey('self', null=True, blank=True, related_name='children')
+
+
+class SubTopic(models.Model):
+    title = models.CharField(max_length = 100)
+    file = models.TextField() # url of the file or content
+    #how to link this url with urls.py??
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+
 
 class Progress(models.Model):
     #Everything we need to model the learning progress
@@ -12,32 +40,3 @@ class Progress(models.Model):
     #subtopic
     #scoreInCurrentTopic
     #overallScore
-
-
-
-class Student(models.Model):
-    firstName = models.CharField(max_length = 50)
-    lastName = models.CharField(max_length = 50)
-    email = models.EmailField()
-    school = models.CharField(max_length = 100)
-
-class Teacher(models.Model):
-    firstName = models.CharField(max_length=50)
-    lastName = models.CharField(max_length=50)
-    email = models.EmailField()
-
-class Course(models.Model):
-    description = models.TextField
-    start = models.OneToOneField(Topic, on_delete=CASCADE)
-
-class Topic(models.Model):
-    title = models.CharField(max_length=100)
-    nextTopic = models.OneToOneField(Topic)
-    subtopic = models.OneToOneField(SubTopic)
-    #content
-
-
-class SubTopic(models.Model):
-    title = models.CharField(max_length = 100)
-    nextSubTopic = models.OneToOneField(SubTopic)
-    #content
